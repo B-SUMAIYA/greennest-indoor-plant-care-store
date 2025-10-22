f0c31a89fc06a2d299c528486e8206aa5fc5e43b
B-SUMAIYA <bintealam08@gmail.com>
2025-10-22 22:40:50 +0600

    updated plant card

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,36 +1,23 @@
 
-// import { Link } from "react-router";
-
-// const PlantCard = ({ plant }) => {
-//   return (
-//     <div className="bg-white rounded-lg shadow p-4 flex flex-col">
-//       <img src={plant.image} alt={plant.plantName} className="h-40 w-full object-cover rounded" />
-//       <div className="mt-3 flex-1">
-//         <h3 className="font-semibold text-lg">{plant.plantName}</h3>
-//         <p className="text-sm text-gray-500">{plant.category} • {plant.careLevel}</p>
-//         <div className="mt-2 flex items-center justify-between">
-//           <div>
-//             <p className="font-medium">${plant.price}</p>
-//             <p className="text-xs text-gray-600">⭐ {plant.rating}</p>
-//           </div>
-//           <Link
-//             to={`/plants/${plant.plantId}`}
-//             className="bg-green-600 text-white px-3 py-1 rounded-md text-sm"
-//           >
-//             View Details
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PlantCard;
 import React from 'react';
+import { Link } from 'react-router';
 const PlantCard = ({plant}) => {
   return (
-    <div>
-      <img src="" alt="" />
+    <div className='bg-white rounded-lg shadow p-4 flex flex-col'>
+      <img src={plant.image} alt="plant.plantName" className='h-40 w-full object-cover rounded' />
+      <div className='mt-3 flex-1'>
+        <h3 className='font-semibold text-lg'>{plant.plantName}</h3>
+        <p className='text-sm text-gray-500'>{plant.category} • {plant.careLevel}</p>
+        <div className="mt-2 flex items-center justify-between">
+          <div>
+            <p className='font-medium'>${plant.price}</p>
+            <p className='text-xs text-gray-600'>{plant.rating}</p>
+            <Link to={`/plants/${plant.plantId}`} className='bg-green-700 text-white px-3 py-1 rounded-md text-sm'>
+              View Details
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
